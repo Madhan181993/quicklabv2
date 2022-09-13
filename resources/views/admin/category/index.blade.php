57a7cfeb2e83d82370db306eb12dbdd62ea55410
sampathrajMP <sampathraj.mp@gmail.com>
2022-09-13 16:31:23 +0530

    updated Category Page, web.php

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -2,24 +2,34 @@
 
 @section('content')
 
-<!-- TODO : Check whether Category needs image. If not, remove image related code -->
+<!-- TODO : Show Parent Field and fetch based on Parent Id from Category Table -->
+
+<style>
+    label {
+        font-size: 14px;
+        font-weight: 500;
+    }
+    input[type="text"],input[type="email"],input[type="password"]{
+        height:30px;
+    }
+</style>
 
 @if (session('status'))
 <div class="alert alert-success">{{ session('status') }}</div>
 @endif
+
+<h4>Category Page</h4>
 <div class="card">
-    <div class="card-header">
-        <h1>Category Page</h1>
-    </div>
     <div class="card-body">
         <a href="{{ url('add-category') }}" class="btn btn-primary button1" data-toggle="modal" data-target="#AddCategoryModal"> Add Category</a>
         <table class="table table-bordered table-striped">
             <thead>
                 <tr>
                     <th>Id</th>
+                    <th>Parent</th>
                     <th>Name</th>
                     <th>Description</th>
-                    <!-- <th>Image</th> -->
+                    <th>Status</th>
                     <th>Action</th>
                 </tr>
             </thead>
@@ -27,14 +37,14 @@
                 @foreach($category as $item)
                 <tr>
                     <td>{{ $item->id }}</td>
-                    <td>{{ $item->name }}</td>
-                    <td>{{ $item->description }}</td>
-                    <!-- <td>
-                        <img src="{{ asset('assets/uploads/category/'.$item->image) }}" class="category-list-img" alt="Image Here">
-                    </td> -->
+                    <td>{{ $item->categoriesparentid }}</td>
+                    <td>{{ $item->categoriesname }}</td>
+                    <td>{{ $item->categoriesdescription }}</td>
+                    <td>{{ $item->status }}</td>
+
                     <td>
                         <button href="#" value="{{ $item->id }}" class="editbtn"><i class="far fa-edit"></i></button>
-                        <button href="#" data-toggle="tooltip" title="Delete"><i class="far fa-trash-alt"></i></button>
+                        <button href="#" data-toggle="tooltip" title="Delete" class="deletebtn"><i class="far fa-trash-alt"></i></button>
                         <!-- <button data-toggle="tooltip" title="Delete"><i class="far fa-trash-alt"></i></button> -->
                         <a href=""></a>
                     </td>
@@ -69,38 +79,20 @@
                             @csrf
                             <div class="row">
                                 <div class="col-md-6  mb-2">
-                                    <label for="">Name</label>
+                                    <label for="">Category Name</label>
                                     <input type="text" class="form-control" name="name" required>
                                     <div class="alert alert-danger" style="display:none"></div>
                                 </div>
                                 <div class="col-md-6 mb-2">
-                                    <label for="">Slug</label>
+                                    <label for="">Category Code</label>
                                     <input type="text" class="form-control" name="slug">
                                 </div>
-                                <div class="col-md-12">
-                                    <textarea name="description" cols="30" rows="3" class="form-control mb-2" placeholder="Description"></textarea>
+                                <div class="col-md-12 mb-2">
+                                    <label for="">Category Description</label>
+                                    <textarea name="meta_description" rows="3" class="form-control"></textarea>
                                 </div>
                                 <div class="col-md-4 mb-2">
                                     <label for="status" class="checkboxLabel">Status&nbsp;&nbsp;<input type="checkbox" class="form-control" name="status"></label>
-                                </div>
-                                <div class="col-md-4 mb-2">
-                                    <label for="popular" class="checkboxLabel">Popular&nbsp;&nbsp; <input type="checkbox" class="form-control" name="popular"></label>
-
-                                </div>
-                                <div class="col-md-6 mb-2">
-                                    <label for="meta_title">Meta Title</label>
-                                    <input type="text" class="form-control" name="meta_title">
-                                </div>
-                                <div class="col-md-6 mb-2">
-                                    <label for="">Meta Keywords</label>
-                                    <input type="text" class="form-control" name="meta_keywords">
-                                </div>
-                                <div class="col-md-12 mb-2">
-                                    <label for="">Meta Description</label>
-                                    <textarea name="meta_description" rows="3" class="form-control"></textarea>
-                                </div>
-                                <div class="col-md-8 mb-3">
-                                    <input type="file" name="image" class="form-control">
                                 </div>
                                 <div class="col-md-12 ">
 
